6033d648d855042ab3d4d772a9019b0014b1e0ce
xa1on <minytale16@gmail.com>
2024-10-19 22:33:43 -0700

    ebayscraper working

---
+++ src/scraper/ebayscraper.py
@@ -0,0 +1,12 @@
+import sys
+sys.path.append('src/scraper/ebayScraper')
+from Spiders.ebay import EbaySpider
+
+
+spider = EbaySpider()
+spider.exec(
+    search_keywords = "book", 
+    pages_to_scrape = 3, #number of pages to scrape (up to 166)
+    output_csv = "Reports/ebay_data.csv",
+    output_json = "Reports/ebay_data.json"
+)
