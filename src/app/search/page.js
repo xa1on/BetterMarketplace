5c2de27819bd31d0e6e3bcf433ba638a0d5a91ce
EwwPhysics <74149971+EwwPhysics@users.noreply.github.com>
2024-10-19 20:48:24 -0700

    Revalidate path every minute (unsure if this works...)

--- src/app/search/page.js
+++ src/app/search/page.js
@@ -1,9 +1,14 @@
 import SearchForm from "./SearchForm.js";
 import Results from "./Results.js";
+import { revalidatePath } from "next/cache.js";
 
 export default function SearchPage(searchParams) {
 
     const product = searchParams.searchParams.product || "";
+
+    setInterval(function() {
+        revalidatePath("/search");
+    }, 1 * 1000); // 60 * 1000 milsec
 
     return (
         <div className="flex flex-col min-h-screen justify-center items-center">
